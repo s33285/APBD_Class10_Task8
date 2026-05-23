c04213b1b95110339029d9763b7bb9f8599b4e85
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-05-23 18:32:35 +0200

    Added ModelExtensions

--- ModelExtensions/Student.Partial.cs
+++ ModelExtensions/Student.Partial.cs
@@ -1,6 +1,11 @@
-﻿namespace UniversityTasksDbFirstApi.ModelExtensions
-{
-    public class Student
+﻿namespace UniversityTasksDbFirstApi.Models { 
+    public partial class Student
     {
+        public string FullName => $"{FirstName} {LastName}";
+
+        public bool HasAcademicEmail()
+        {
+            return Email.EndsWith("@students.example.edu", StringComparison.OrdinalIgnoreCase);
+        }
     }
 }
